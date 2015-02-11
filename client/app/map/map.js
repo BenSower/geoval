c9f7c1635f1e5bdb194da20553800871b0a29461
Ben Sower <sauer.benjamin@gmail.com>
2015-02-11 23:44:45 +0100

    map page created

---
+++ client/app/map/map.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('geovalApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('map', {
+        url: '/map',
+        templateUrl: 'app/map/map.html',
+        controller: 'MapCtrl'
+      });
+  });
