ed4d9e810247a9537d69b83f15fbea436e02856e
Ben Sower <sauer.benjamin@gmail.com>
2015-02-12 15:37:39 +0100

    added mousewheelscroll

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -6,15 +6,26 @@ angular.module('geovalApp')
             center: {
                 lat: 0,
                 lon: 0,
-                autodiscover: true,  
+                autodiscover: true,
                 projection: 'EPSG:4326'
             },
             defaults: {
+                layers: {
+                    main: {
+                        source: {
+                            type: 'OSM',
+                            url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png'
+                        }
+                    }
+                },
                 interactions: {
                     mouseWheelZoom: true
+                },
+                controls: {
+                    zoom: false,
+                    rotate: false,
+                    attribution: false
                 }
             }
-
         });
     });
- 
