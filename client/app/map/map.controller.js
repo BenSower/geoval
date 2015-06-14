e1c6ae214f38880b0d6aeca9437cf7b3b097f536
BenSower <sauer.benjamin@gmail.com>
2015-06-14 17:51:03 +0200

    added thresholdvalue to trajectories in db

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -117,67 +117,47 @@ angular.module('geovalApp')
             var filteredTrajectories = filterTrajectories(trajectories);
             $scope.layers[1].source.geojson.object.features = filteredTrajectories;
             $scope.sliderOptions.renderedTrajectories = filteredTrajectories.length;
-            updateMarkers(filteredTrajectories);         
+            updateMarkers(filteredTrajectories);
             return filteredTrajectories;
         }
 
-        function updateMarkers(trajectories){
+        function updateMarkers(trajectories) {
             //reset markers
             markers = [];
             $scope.markers = [];
             for (var i = 0; i < trajectories.length; i++) {
                 var traj = trajectories[i];
-                var labelMessage = '<h5>' + traj.id + '</h5>'  
-                                    + 'Coordinates: ' +  traj.geometry.coordinates.length;
-                var marker = {
-                    name: traj.id,
-                    lat: traj.geometry.coordinates[0][1],
-                    lon: traj.geometry.coordinates[0][0],
-                    label: {
-                        message: labelMessage,
-                        show: false,
-                        showOnMouseOver: true
-                    }
-                };
-                markers.push(marker);
+                if (traj.geometry.coordinates[0]) {
+                    var labelMessage = '<h5>' + traj.id + '</h5>' + 'Coordinates: ' 
+                                    + traj.geometry.coordinates.length 
+                                    + '<br/> Outlier Threshold: ' + traj.properties.outlierThreshold + 'm';
+                    var marker = {
+                        name: traj.id,
+                        lat: traj.geometry.coordinates[0][1],
+                        lon: traj.geometry.coordinates[0][0],
+                        label: {
+                            message: labelMessage,
+                            show: false,
+                            showOnMouseOver: true
+                        }
+                    };
+                    markers.push(marker);
+                } else {
+                    console.log("this trajectory has no correct coordinates:", traj)
+                }
+
             }
 
-             $scope.toggleMarkers();
+            $scope.toggleMarkers();
         }
 
-
-        function deg2rad(deg) {
-            return deg * (Math.PI / 180);
-        }
-
-        function getDistanceFromLonLatInKm(lon1, lat1, lon2, lat2) {
-            var R = 6371; // Radius of the earth in km
-            var dLat = deg2rad(lat2 - lat1); // deg2rad below
-            var dLon = deg2rad(lon2 - lon1);
-            var a =
-                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-                Math.sin(dLon / 2) * Math.sin(dLon / 2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c; // Distance in km
-            return d;
-        }
 
         //drops all trajectories with a outlier
         function simpleOutlierRemoval(trajectory) {
-
-            var threshold = $scope.sliderOptions.thresholdValue / 1000; //translate m to km
-            //calculate distances between every coordinate and the next one
-            for (var i = 0; i < trajectory.geometry.coordinates.length - 1; i++) {
-                var firstCoordinate = trajectory.geometry.coordinates[i];
-                var secondCoordinate = trajectory.geometry.coordinates[i + 1];
-                var distance = getDistanceFromLonLatInKm(firstCoordinate[0], firstCoordinate[1], secondCoordinate[0], secondCoordinate[1]);
-                //drop trajectory if a single distance between two points is bigger than the threshold
-                if (distance >= threshold) {
-                    return null;
-                }
+            var threshold = $scope.sliderOptions.thresholdValue; 
+            if (trajectory.properties.outlierThreshold > threshold) {
+                return null;
             }
-
             return trajectory;
         }
 
