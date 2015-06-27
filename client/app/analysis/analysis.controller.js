f550118debac12eb88ee26bac8b84ee660c078a6
bensower <sauer.benjamin@gmail.com>
2015-06-27 16:33:55 +0200

    added table listing

--- client/app/analysis/analysis.controller.js
+++ client/app/analysis/analysis.controller.js
@@ -1,12 +1,8 @@
 'use strict';
-
 angular.module('geovalApp')
-    .controller('AnalysisCtrl', function($scope, $http) {
-
-        if ($scope.rawTrajectories === undefined) {
-            $http.get('/api/trajectories').success(function(trajectories) {
-                $scope.rawTrajectories = trajectories;
-                console.log(trajectories[0]);
-            });
-        } 
+    .controller('AnalysisCtrl', function ($scope, $http) {
+        $http.get('/api/trajectories').success(function(trajectories) {
+            $scope.rawTrajectories = trajectories;
+            console.log(trajectories[0]);
+        });
     });
