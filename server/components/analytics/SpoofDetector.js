018a577bc6288fe42a64d8c044007f455563c3e6
bensower <sauer.benjamin@gmail.com>
2015-10-07 12:07:23 +0200

    refactored and added printnig

--- server/components/analytics/SpoofDetector.js
+++ server/components/analytics/SpoofDetector.js
@@ -1,46 +1,94 @@
+'use strict';
+var model = {
+    avrgBucketCount: 0
+};
+var results = {
+    spoofCount: 0,
+    spoofs: [],
+    realTrajectories: []
+};
+
+var trainingAlgorithms = [bucketTraining];
+var detectionAlgorithms = [bucketDetection];
+
 function SpoofDetector() {}
 
-var model = {avrgBucketCount : 0};
-var results = {spoofCount : 0, spoofs : [], noSpoofs : []};
-
 SpoofDetector.prototype.detectSpoofs = function(trajectories, spoofs) {
-	
-	console.log("detecting Spoofs");
 
-	trainModel(trajectories);
-	analyseSpoofs(spoofs);
-	return this.results;
+    console.log("detecting Spoofs");
+
+    trainModel(trajectories);
+    analyseTrajectories(spoofs);
+    analyseTrajectories(trajectories);
+    printResults(results);
+    return this.results;
 }
 
 /*
 	Trains the model by analysing real trajectories
 */
-function trainModel(trajectories){
-	console.log('Training the model with real trajectories');
-	
-	var bucketCount = -1;
-	var biggestBucket = -1;
-	var smallestBucket = -1;
+function trainModel(trajectories) {
+    console.log('Training the model with real trajectories');
+    for (var i = 0; i < trainingAlgorithms.length; i++) {
+        trainingAlgorithms[i](trajectories);
+    }
+}
 
-	for (var i = 0; i < trajectories.length; i++){
-		var trajectory = trajectories[i];
-		var distribution = trajectory.featureVector.distribution;
-		bucketCount += Object.keys(distribution).length;
-	}
-	model.avrgBucketCount = bucketCount/trajectories.length;
+/**
+* Applies detectionAlgorithms to the trajectories
+*/
+function analyseTrajectories(trajectories) {
+    for (var i = 0; i < detectionAlgorithms.length; i++) {
+        for (var h = 0; h < trajectories.length; h++) {
+            var trajectory = trajectories[h];
+            if (detectionAlgorithms[i](trajectory).isSpoof) {
+                results.spoofs.push(trajectory);
+            } else {
+                results.realTrajectories.push(trajectory);
+            };
+        }
+    }
+}
+
+function printResults() {
+    console.log('Number of spoofs:', results.spoofs.length);
+    console.log('Number of real trajectories:', results.realTrajectories.length);
 }
 
 
-function analyseSpoofs(spoofs){
-	console.log('Looking for spoofs');
-	for (var i = 0; i < spoofs.length; i++){
-		var spoof = spoofs[i];
-		if (Object.keys(spoof.featureVector.distribution).length > model.avrgBucketCount){
-			results.spoofs.push(spoof);
-		} else {
-			results.noSpoofs.push(spoof);
-		}
-	}
-	console.log(results.spoofs.length, results.noSpoofs.length);
+
+/**
+######################################################################################################################
+											Training Algos
+######################################################################################################################
+*/
+function bucketTraining(trajectories) {
+    var bucketCount = -1;
+    var biggestBucket = -1;
+    var smallestBucket = -1;
+
+    for (var i = 0; i < trajectories.length; i++) {
+        var trajectory = trajectories[i];
+        var distribution = trajectory.featureVector.distribution;
+        bucketCount += Object.keys(distribution).length;
+    }
+    model.avrgBucketCount = bucketCount / trajectories.length;
 }
+
+
+/**
+######################################################################################################################
+											Detection Algos
+									An algo needs to return and object like {isSpoof : true}
+######################################################################################################################
+*/
+
+/*
+	Returns false, if number of buckets > model number of buckets
+*/
+function bucketDetection(trajectory) {
+	//just to make sure the result is interpreted right by the coder...
+    return {isSpoof : (Object.keys(trajectory.featureVector.distribution).length > model.avrgBucketCount)};
+}
+
 module.exports = new SpoofDetector();
