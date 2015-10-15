a881cbf97774f14b7340b3a231afcfafd8a61041
bensower <sauer.benjamin@gmail.com>
2015-10-15 19:36:28 +0200

    refactoring

--- server/components/analytics/SpoofDetector.js
+++ server/components/analytics/SpoofDetector.js
@@ -3,11 +3,6 @@ var Table = require('cli-table');
 
 
 SpoofDetector.prototype.model = {
-    buckets: {
-        avrgMinBucket: -1,
-        avrgMaxBucket: -1,
-        avrgBucketCount: -1
-    },
     sampleCount: {
         avrgSampleCount: -1,
         minSampleCount: -1,
@@ -128,40 +123,94 @@ SpoofDetector.prototype.presentResults = function(results, spoofLvL) {
 
 }
 
+function compareIntMaps(vectorA, vectorB) {
 
+    var misses = 0;
+    var diffs = []
 
+    for (var key in vectorA) {
+        if (!vectorA.hasOwnProperty(key)) {
+            continue;
+        }
+        var diff;
+        if (vectorB[key] !== undefined) {
+            var localVal = vectorA[key];
+            var modelVal = vectorB[key];
+            var diff = (localVal > modelVal) ? modelVal / localVal : localVal / modelVal;
+            //perceńtage of how close the values are to each other in this bucket
+            diffs.push(diff * 100);
+        } else {
+            misses++;
+        }
+    }
+
+    var sum = diffs.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+
+    var possibleKeys = Object.keys(vectorA).length;
+    var p = (sum / possibleKeys);
+
+    return {
+        p: p,
+        misses: misses
+    };
+}
+
+function setTimeDistribution(model, trajectories) {
+
+    model.timeDifference = (typeof model.timeDifference === 'undefined') ? {} : model.timeDifference;
+    model.timeDifference.absoluteDistribution = (typeof model.timeDifference.absoluteDistribution === 'undefined') ? {} : model.timeDifference.absoluteDistribution;
+    model.timeDifference.normalizedDistribution = (typeof model.timeDifference.normalizedDistribution === 'undefined') ? {} : model.timeDifference.normalizedDistribution;
+
+    for (var i = 0; i < trajectories.length; i++) {
+        var trajectory = trajectories[i];
+        for (var j = 0; j < trajectory.geometry.coordinates.length - 1; j++) {
+            var coordinateA = new Date(trajectory.properties.coordTimes[j]);
+            var coordinateB = new Date(trajectory.properties.coordTimes[j + 1]);
+            var diff = coordinateB.getTime() - coordinateA.getTime();
+            //initialize or increment
+            model.timeDifference.absoluteDistribution[diff] = model.timeDifference.absoluteDistribution[diff] + 1 || 1;
+        }
+    }
+    model.timeDifference.normalizedDistribution = getNormalizedDistribution(model.timeDifference.absoluteDistribution, trajectories);
+    return model;
+}
+
+function getNormalizedDistribution(absoluteDistribution, trajectories) {
+    var normalizedDistribution = absoluteDistribution;
+    if (trajectories.length > 1) {
+        //normalizing/averaging every bucket value
+        for (var bucket in normalizedDistribution) {
+            if (normalizedDistribution.hasOwnProperty(bucket)) {
+                normalizedDistribution[bucket] = normalizedDistribution[bucket] / trajectories.length;
+            }
+        }
+    }
+    return normalizedDistribution;
+}
 /**
 ######################################################################################################################
                                             Training Algos
 ######################################################################################################################
 */
 function bucketTraining(model, trajectories) {
-    var bucketCount = 0;
-    var minBucketSum = 0;
-    var maxBucketSum = 0;
 
-    var minRed = function(a, b) {
-        if (b === 'biggestDistance')
-            return a;
-        else
-            return Math.min(a, b);
-    };
+    model.buckets = (typeof model.buckets === 'undefined') ? {} : model.buckets;
+    model.buckets.absoluteDistribution = (typeof model.buckets.absoluteDistribution === 'undefined') ? {} : model.buckets.absoluteDistribution;
+    model.buckets.normalizedDistribution = (typeof model.buckets.normalizedDistribution === 'undefined') ? {} : model.buckets.normalizedDistribution;
 
     for (var i = 0; i < trajectories.length; i++) {
         var trajectory = trajectories[i];
-        var distribution = trajectory.featureVector.distribution;
-        bucketCount += Object.keys(distribution).length;
-        maxBucketSum += distribution.biggestDistance;
-        //console.log(distribution.biggestDistance);
-        var buckets = Object.keys(distribution);
-        var smallesBucket = buckets.reduce(minRed);
-
-        minBucketSum += smallesBucket;
+        for (var key in trajectory.featureVector.distribution) {
+            //initialize or increment
+            if (key !== 'biggestDistance') {
+                model.buckets.absoluteDistribution[key] = model.buckets.absoluteDistribution[key] + 1 || 1;
+            }
+        }
     }
-    model.buckets.avrgBucketCount = bucketCount / trajectories.length;
-    model.buckets.avrgMinBucket = Math.min(0, minBucketSum / trajectories.length);
-    model.buckets.avrgMaxBucket = maxBucketSum / trajectories.length;
-    //console.log(model);
+    model.buckets.normalizedDistribution = getNormalizedDistribution(model.buckets.absoluteDistribution, trajectories);
+
     return model;
 }
 
@@ -180,94 +229,30 @@ function timeTraining(model, trajectories) {
 ######################################################################################################################
 */
 
-function setTimeDistribution(model, trajectories) {
-
-    model.timeDifference = (typeof model.timeDifference === 'undefined') ? {} : model.timeDifference;
-    model.timeDifference.absoluteDistribution = (typeof model.timeDifference.absoluteDistribution === 'undefined') ? {} : model.timeDifference.absoluteDistribution;
-    model.timeDifference.normalizedDistribution = (typeof model.timeDifference.normalizedDistribution === 'undefined') ? {} : model.timeDifference.normalizedDistribution;
-
-    for (var i = 0; i < trajectories.length; i++) {
-        var trajectory = trajectories[i];
-        for (var j = 0; j < trajectory.geometry.coordinates.length - 1; j++) {
-            var coordinateA = new Date(trajectory.properties.coordTimes[j]);
-            var coordinateB = new Date(trajectory.properties.coordTimes[j + 1]);
-            var diff = coordinateB.getTime() - coordinateA.getTime();
-            //initialize or increment
-            model.timeDifference.absoluteDistribution[diff] = model.timeDifference.absoluteDistribution[diff] + 1 || 1;
-            model.timeDifference.normalizedDistribution[diff] = model.timeDifference.absoluteDistribution[diff] / trajectories.length;
-        }
-    }
-    return model;
-
-}
 /*
     Counts and averages the amount of samples in a trajectory
 */
 function timeDetection(model, trajectory) {
 
     var localModel = setTimeDistribution({}, [trajectory]);
-    var misses = 0;
-    var timeIntervalCount = Object.keys(localModel.timeDifference.absoluteDistribution).length;
-
-    for (var key in localModel.timeDifference.absoluteDistribution) {
-        if (!localModel.timeDifference.absoluteDistribution.hasOwnProperty(key)) {
-            continue;
-        }
-        var diff;
-        var diffs = []
-        if (model.timeDifference.absoluteDistribution[key] !== undefined) {
-            var localVal = localModel.timeDifference.absoluteDistribution[key];
-            var modelVal =  model.timeDifference.absoluteDistribution[key];
-            var diff = (localVal > modelVal) ? modelVal/localVal : localVal/modelVal;
-            diffs.push(diff);
-        }else {
-            misses++;
-        }
-    }
-    //console.log(misses + '/' + timeIntervalCount + ' missed.')
-
-    var sum = diffs.reduce(function(a,b){
-        return a+b;
-    }, 0);
-    var p = (sum / timeIntervalCount) * 100;
-
+    //console.log(localModel.timeDifference.absoluteDistribution);
+    var comparisonResult = compareIntMaps(localModel.timeDifference.absoluteDistribution, model.timeDifference.normalizedDistribution);
     return {
-        isSpoof: p < 0.001,
-        p: p
+        isSpoof: comparisonResult.p < 1.5,
+        p: comparisonResult.p
     };
-
 }
-
-
 
 /*
     Returns false, if number of buckets > model number of buckets
 */
 function bucketDetection(model, trajectory) {
-    var bucketCount = Object.keys(trajectory.featureVector.distribution).length;
 
-    var isInAvrgRange = (bucketCount > model.buckets.avrgMinBucket && bucketCount < model.buckets.avrgMaxBucket);
-    var maxDistToMedian = Math.max(model.buckets.avrgBucketCount - model.buckets.avrgMinBucket, model.buckets.avrgMaxBucket - model.buckets.avrgBucketCount);
-    //probability in percent, that the trajectory is real
-    var p = 0;
-    if (isInAvrgRange) {
-        //p = (maxDistToMedian - bucketCount) / maxDistToMedian * 100;
-        p = Math.pow((maxDistToMedian - bucketCount) / maxDistToMedian, 2) * 100;
-    }
-    /*
-    console.log('bucketcount', bucketCount);
-    console.log('minbucket', model.buckets.avrgMinBucket);
-    console.log('maxbucket', model.avrgMaxBucket);
-    console.log('model.buckets.avrgBucketCount', model.buckets.avrgBucketCount);
-    console.log('maxDistToMedian', maxDistToMedian);
-    console.log('Probability', p);
-    */
+    var comparisonResult = compareIntMaps(trajectory.featureVector.distribution, model.buckets.normalizedDistribution);
 
-
-    //just to make sure the result is interpreted right by the coder...
     return {
-        isSpoof: p < 1,
-        p: p
+        isSpoof: comparisonResult.p < 7,
+        p: comparisonResult.p
     };
 }
 
